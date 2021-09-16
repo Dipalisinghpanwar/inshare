2a4b8dfc1492bb347f8fbc36b955afafa52c8c5b
dsdipali21 <83289846+Dipalisinghpanwar@users.noreply.github.com>
2021-09-16 16:04:40 +0530

    Update server.js

--- file_sharing/server.js
+++ file_sharing/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const cors = require('cors');
+
 
 PORT = process.env.PORT || 3000
 
@@ -13,21 +13,7 @@ app.use(express.json()) //express middleware
 //database
 require('./config/db')
 
-// Cors 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
 
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
-
-app.use(cors(corsOptions))
 
 //Template engine
 app.set('views', path.join(__dirname, '/views'));
